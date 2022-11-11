95f7a9ffc2a45ecd8e2805bca7f017c286f0d233
Kazi Tajnur Islam <kazi.tajnur@vivacomsolutions.com>
2022-11-11 20:22:27 +0600

    Login page design complete

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { FC, Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '../routes/mainRoutes';
+import 'antd/dist/antd.css';
 
 const loading = (
   <div>
--- src/features/Auth/Login2/Login.tsx
+++ src/features/Auth/Login2/Login.tsx
@@ -1,13 +1,14 @@
-import { FC, FormEvent } from 'react';
+import { FC } from 'react';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Form, Input, Button, Typography } from 'antd';
 import companyImage from '../../../assets/images/azerion.png';
 import './Login.css';
 
+const { Text } = Typography;
+
 const Login: FC = () => {
-
-  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    console.log('Login clicked');
+  const handleLogin = (values: any) => {
+    console.log('Success:', values);
   }
 
   return (
@@ -17,28 +18,45 @@ const Login: FC = () => {
           <img className="company-image" src={companyImage} alt="azerion" />
         </div>
 
-        <div className="welcome-section">
-          <h2>Login</h2>
-          <p>Sign In to your account</p>
+        <div className="login-text-container">
+          <Text className="login-title">Login</Text>
+          <Text className="login-subtitle">Sign In to your account</Text>
         </div>
 
-        <form onSubmit={handleLogin}>
-          <div className='login-input'>
-            <p>Email: </p>
-            <div>
-              <input type="text" placeholder='Enter email' />
-            </div>
-          </div>
-          <div className='login-input'>
-            <p>Password: </p>
-            <div>
-              <input type="password" placeholder='Enter password' />
-            </div>
-          </div>
-          <div className='login-input'>
-            <button type="submit"> Login </button>
-          </div>
-        </form>
+        <Form
+          name="normal_login"
+          className="login-form"
+          onFinish={handleLogin}
+        >
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: 'Please enter your Email!', type: 'email' }]}
+            className="mb-4"
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon login-icon" />}
+              placeholder="Email"
+              className="login-input"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please enter your Password!' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon login-icon" />}
+              type="password"
+              placeholder="Password"
+              className="login-input"
+            />
+          </Form.Item>
+
+          <Form.Item className="mb-0">
+            <Button type="primary" htmlType="submit" className="login-button">
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   );
