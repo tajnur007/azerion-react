10c4e39be8397ee15db2f9f8a1155a9682c8dd7f
Kazi Tajnur Islam <kazi.tajnur@vivacomsolutions.com>
2022-11-14 15:46:55 +0600

    My campaign upper porttion partially complete

--- src/features/MyCampaigns/MyCampaigns.tsx
+++ src/features/MyCampaigns/MyCampaigns.tsx
@@ -1,10 +1,41 @@
 import { FC } from 'react';
+import { Space, Input, Button, Row, Col, Typography } from 'antd';
 import './MyCampaigns.css';
 
+const { Search } = Input;
+const { Text } = Typography;
+
 const MyCampaigns: FC = () => {
+  const onSearch = (value: string) => console.log(value);
+
   return (
     <div className="campaign-container">
+      <Text className="campaign-heading" strong> My Campaigns </Text>
 
+      <div className="search-container">
+        <Search placeholder="Search" allowClear onSearch={onSearch} style={{ width: 200 }} />
+        <Button type="primary" shape="round" ghost>
+          Filters
+        </Button>
+      </div>
+
+
+      <Row gutter={16}>
+        <Col span={4}>
+          <Text className="filter-title" strong> Platform: </Text>
+        </Col>
+        <Col span={4}>
+          <Text className="filter-title" strong> Business Unit: </Text>
+        </Col>
+        <Col span={4}>
+          <Text className="filter-title" strong> Status: </Text>
+        </Col>
+        <Col span={4}>
+          <Button type="primary" shape="round" ghost>
+            Clear All
+          </Button>
+        </Col>
+      </Row>
     </div>
   )
 }
